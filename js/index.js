353c501ec09209092b67e60f8550d74b2aa1a4a5
Krzysztof Burka <kazcorro@gmail.com>
2017-05-29 19:36:35 +0200

    5

--- js/index.js
+++ js/index.js
@@ -39,6 +39,36 @@ $(window).scroll(function() {
       
   });
     
+    //slider
+   var slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+    showDivs(slideIndex += n);
+    console.log("hejo");
+}
+    var right=document.querySelector("#right");
+    right.addEventListener('click',function(){
+        plusDivs(1);
+    });
+    
+     var left=document.querySelector("#left");
+    left.addEventListener('click',function(){
+        plusDivs(-1);
+    });
+
+function showDivs(n) {
+    var i;
+    var x = document.getElementsByClassName("mySlides");
+    if (n > x.length) {slideIndex = 1} 
+    if (n < 1) {slideIndex = x.length} ;
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none"; 
+    }
+    x[slideIndex-1].style.display = "block"; 
+    
+};
+    //--slider
     
  $("#fun").fadeIn(700,function(){
    $("#passion").fadeIn(700,function(){
